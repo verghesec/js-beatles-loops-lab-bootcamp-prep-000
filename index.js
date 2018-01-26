cbaf8044c8cecd2d177c62765e54125fe8ea4dc2
Cecil Verghese <cecil.verghese@gmail.com>
2018-01-26 05:01:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 
 function theBeatlesPlay(musicians, instruments) {
-
+  var introArray = [];
+  for (let i=0; i<musicians.length; i++) {
+    introArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
 }
